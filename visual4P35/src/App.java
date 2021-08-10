5a6b808445284199369a9f4174385a98d66df9b5
marinacharris <85807129+marinacharris@users.noreply.github.com>
2021-08-10 04:24:05 -0500

    Add files via upload

---
+++ visual4P35/src/App.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Hola mundo");
+        JOptionPane.showInputDialog(null, "Digite un valor");
+        
+    }
+}
